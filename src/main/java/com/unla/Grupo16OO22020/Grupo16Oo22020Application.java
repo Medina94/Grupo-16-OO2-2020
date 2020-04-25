968802e1dbe6e7f5cc29332231e92b705b62e067
Cristian <cristian.medina94@gmail.com>
2020-04-25 20:50:50 -0300

    Primer commit

---
+++ src/main/java/com/unla/Grupo16OO22020/Grupo16Oo22020Application.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo16OO22020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo16Oo22020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo16Oo22020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/Grupo16OO22020/Grupo16Oo22020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo16OO22020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo16Oo22020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
