da20c8dfd657d56a9206efe1fb35aa17738c4c16
Cristian <cristian.medina94@gmail.com>
2020-05-13 00:02:08 -0300

    capa de acceso a datos IClienteRepository e IEmpleadoRepository

---
+++ src/main/java/com/unla/Grupo16OO22020/repositories/IClienteRepository.java
@@ -0,0 +1,20 @@
+package com.unla.Grupo16OO22020.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unla.Grupo16OO22020.entities.Cliente;
+
+@Repository("clienteRepository")
+public interface IClienteRepository extends JpaRepository<Cliente, Serializable> {
+	public abstract Cliente findById(int id);
+
+	//public abstract Cliente findByName(String name);
+
+	// Todas las personas que tengan un título con ese nombre (parámetro name)
+	// @Query("SELECT p FROM Person p JOIN FETCH p.degrees d WHERE d.name =
+	// (:name)")
+	// public abstract List<Cliente> findByDegreeName(String name);
+}
---
+++ src/main/java/com/unla/Grupo16OO22020/repositories/IEmpleadoRepository.java
@@ -0,0 +1,16 @@
+package com.unla.Grupo16OO22020.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unla.Grupo16OO22020.entities.Cliente;
+import com.unla.Grupo16OO22020.entities.Empleado;
+
+@Repository("empleadoRepository")
+public interface IEmpleadoRepository extends JpaRepository<Empleado, Serializable>{
+	public abstract Empleado findById(int id);
+
+	//public abstract Empleado findByName(String name);
+}
