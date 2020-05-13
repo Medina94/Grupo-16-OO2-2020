bf5abc6785c65029fc4945bf28bb5904f300dd81
Cristian <cristian.medina94@gmail.com>
2020-05-13 00:01:11 -0300

    creacion de Model Persona, cliente y empleado

---
+++ src/main/java/com/unla/Grupo16OO22020/models/ClienteModel.java
@@ -0,0 +1,33 @@
+package com.unla.Grupo16OO22020.models;
+
+import java.time.LocalDate;
+
+import com.unla.Grupo16OO22020.entities.Cliente;
+
+public class ClienteModel extends PersonaModel{
+	private int telefono;
+
+	public ClienteModel() {}
+	
+	public ClienteModel(int id, String nombre, String apellido, LocalDate fechaNacimiento, String mail, int telefono) {
+		super(id, nombre, apellido, fechaNacimiento, mail);
+		this.telefono = telefono;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
+	public ClienteModel build(Cliente entidad) {
+		this.setId(entidad.getId());
+		this.setNombre(entidad.getNombre());
+		this.setApellido(entidad.getApellido());
+		this.setFechaNacimiento(entidad.getFechaNacimiento());
+		this.setMail(entidad.getMail());
+		this.telefono = entidad.getTelefono();
+		return this;
+	}
+}
