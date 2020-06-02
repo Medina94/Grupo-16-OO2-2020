595c7ed4122e73ab42399313de8d8986b2cff882
Cristian <cristian.medina94@gmail.com>
2020-06-02 15:54:24 -0300

    Querys para consulta de locales con producto "x" y busqueda por producto y local

--- src/main/java/com/unla/Grupo16OO22020/repositories/ILocalRepository.java
+++ src/main/java/com/unla/Grupo16OO22020/repositories/ILocalRepository.java
@@ -1,8 +1,10 @@
 package com.unla.Grupo16OO22020.repositories;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.unla.Grupo16OO22020.entities.Local;
@@ -11,4 +13,8 @@ import com.unla.Grupo16OO22020.entities.Local;
 public interface ILocalRepository extends JpaRepository<Local, Serializable>{
 
 	public abstract Local findById(int id);	
+	
+	@Query(value="select l.* from local l inner join producto p on l.id = p.local_id where p.descripcion = (:descripcion)", nativeQuery = true)
+	public abstract List<Local> getByStock(String descripcion);
+	
 }
